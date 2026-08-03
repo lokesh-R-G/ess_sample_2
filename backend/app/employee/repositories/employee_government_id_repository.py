6624cac13f695ffec89a8776dac243813d993371
lokesh-r-g <lokeshca2004@gmail.com>
2026-08-03 10:47:36 +0530

    feat: create EmployeeGovernmentIdRepository for managing government ID records

--- backend/app/employee/repositories/employee_government_id_repository.py
+++ backend/app/employee/repositories/employee_government_id_repository.py
@@ -1,6 +1,8 @@
 from app.employee.repositories.base_repository import BaseRepository
 from app.employee.models.employee_government_id import EmployeeGovernmentIdModel
 
+from motor.motor_asyncio import AsyncIOMotorDatabase
+
 class EmployeeGovernmentIdRepository(BaseRepository[EmployeeGovernmentIdModel]):
-    def __init__(self):
-        super().__init__(EmployeeGovernmentIdModel, "employee_government_ids")
+    def __init__(self, db: AsyncIOMotorDatabase):
+        super().__init__(db, "employee_government_ids", EmployeeGovernmentIdModel)
